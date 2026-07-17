b8cebd726774c99cfbc4666c446ca107b83f4ee6
bobbytri2272 <110839341+bobbytri2272@users.noreply.github.com>
2026-07-17 23:14:41 +0700

    Create Anti-Check-Crack.bat

---
+++ Anti-Check-Crack.bat
@@ -0,0 +1,31 @@
+@echo off
+cls
+color 0A
+echo +========================================================+
+echo ^|               POWERSHELL SECURITY CONTROL            ^|
+echo +========================================================+
+echo ^|                                                        ^|
+echo ^|  [0] Block PowerShell (Constrained Language Mode)    ^|
+echo ^|  [1] Unblock PowerShell (Restore Default Mode)       ^|
+echo ^|                                                      ^|
+echo +========================================================+
+echo.
+set /p userinput= ^> Nhap lua chon cua ban (0 hoac 1): 
+
+if "%userinput%"=="0" goto op_block
+if "%userinput%"=="1" goto op_unblock
+exit
+
+:op_block
+setx /M __PSLockdownPolicy 4
+echo.
+echo.
+pause
+exit
+
+:op_unblock
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v __PSLockdownPolicy /f
+echo.
+echo.
+pause
+exit
